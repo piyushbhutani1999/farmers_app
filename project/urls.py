52b2478d886244004af5cf8329dc24eb68000d75
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-09-12 03:03:50 +0530

    base url updated

--- project/urls.py
+++ project/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("product/", include("product.urls")),
-    path("accounts/", include('accounts.urls')),
+    path("", include("product.urls")),
+    path("account/", include('accounts.urls')),
 ]
 
 if settings.DEBUG:
